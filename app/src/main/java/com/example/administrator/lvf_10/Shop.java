8fc66b65750cd78ff0c627bd327d60704e92e139
HUDD-WUA <15010911018@163.com>
2017-04-28 15:59:42 +0800

    First Version in dada-branch
    finish the basic look of “shop”

--- app/src/main/java/com/example/administrator/lvf_10/Shop.java
+++ app/src/main/java/com/example/administrator/lvf_10/Shop.java
@@ -1,7 +1,13 @@
 package com.example.administrator.lvf_10;
 
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+
 
 public class Shop extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class Shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        setCustomActionBar();
+        if(Build.VERSION.SDK_INT>=21){
+            getSupportActionBar().setElevation(1);
+        }
+    }
+
+    private void setCustomActionBar() {
+        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+        View mActionBarView = LayoutInflater.from(this).inflate(R.layout.shop_action_bar, null);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(mActionBarView, lp);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 }
