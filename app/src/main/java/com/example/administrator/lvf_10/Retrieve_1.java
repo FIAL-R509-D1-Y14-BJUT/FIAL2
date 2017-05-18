961c378862191da5e7aef514024764a801d1076a
14080224LiShuang <lslhjcl@163.com>
2017-05-18 23:47:28 +0800

    LvF_1.0
    京辉最开始的版本

---
+++ app/src/main/java/com/example/administrator/lvf_10/Retrieve_1.java
@@ -0,0 +1,13 @@
+package com.example.administrator.lvf_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Retrieve_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_1);
+    }
+}
